1485abd611679ef5de00616090c927f93eac11ca
hedi31 <heinrichhedviganiko@gmail.com>
2020-03-28 00:14:42 +0200

    styling1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
           </MDBContainer>
         </div>
         <div className="row">
-          <h1>About</h1>
+          <h4>About</h4>
         </div>
         <div className="row">
           <h4>Help</h4>
@@ -46,47 +46,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// const Footer = () => {
-//     return (
-//       <div className="main-footer">
-//         <div className="container">
-//           <div className="row">
-//             {/* Colum1 */}
-//             <div className="col-sm">
-//               <h1>Movie website</h1>
-//             </div>
-//             {/* Colum2 */}
-//             <div className="col-sm">
-//               <h1>Movie website</h1>
-//               <ul className="list-unstyled">
-//                 <a href="#!" className="fb-ic mr-3">
-//                   <MDBIcon fab icon="facebook-f" />
-//                 </a>
-//                 <li>b</li>
-//                 <li>c</li>
-//               </ul>
-//             </div>
-//             {/* Colum3 */}
-//             <div className="col-sm">
-//               <h1>Movie website</h1>
-//               <ul className="list-unstyled">
-//                 <li>d</li>
-//                 <li>e</li>
-//                 <li>f</li>
-//               </ul>
-//             </div>
-//           </div>
-//           <hr />
-//           <div className="row">
-//             <p className="col-sm">
-//               &copy;{new Date().getFullYear} Movie Website | All Right Reserved To
-//               Saturn Team |Privacy Policy
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default Footer;
