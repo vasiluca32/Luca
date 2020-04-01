e0cd1e66271d183c47e22c3baaa84536ed2e5719
vasiluca32 <vasilelucaluca@gmail.com>
2020-04-01 20:40:15 +0300

    functionality

--- src/components/main/rating-carousel/Carousel.js
+++ src/components/main/rating-carousel/Carousel.js
@@ -31,17 +31,21 @@ class Carousel extends Component {
     }
 
 
-    handleClick = () => {
-        console.log("test1234");
+    handleRight = () => {
+
         const { currentMovieIndex, results } = this.state;
 
         if (currentMovieIndex < results.length - 1) {
             this.setState({ currentMovieIndex: currentMovieIndex + 6 })
-        } else if (currentMovieIndex > 0) {
-            this.setState({ currentMovieIndex: currentMovieIndex - 6 })
+
         }
 
-
+    }
+    handleLeft = () => {
+        const { currentMovieIndex, results } = this.state;
+        if (currentMovieIndex > 0) {
+            this.setState({ currentMovieIndex: currentMovieIndex - 6 })
+        }
     }
 
 
@@ -50,7 +54,6 @@ class Carousel extends Component {
         console.log(this.state)
         const { currentMovieIndex, results } = this.state;
 
-        const currentMoviesDisplayed = results[currentMovieIndex];
         return (
 
 
@@ -60,9 +63,9 @@ class Carousel extends Component {
             <div class="card-deck" >
 
                 <div class="arrows" role="presentation">
-                    <div class="arrow-wrap" onClick={this.handleClick} role="presentation"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="arrow" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path></svg></div>
+                    <div class={currentMovieIndex === 0 ? "disabled" : "arrow-wrap"} onClick={this.handleLeft} role="presentation"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="arrow" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path></svg></div>
                 </div>
-                {this.state.results.sort(function (a, b) { return b.imdbRating - a.imdbRating }).slice(0, 6).map((res) => {
+                {this.state.results.sort(function (a, b) { return b.imdbRating - a.imdbRating }).slice(currentMovieIndex, currentMovieIndex + 6).map((res) => {
                     return (
 
                         <div class="card">
@@ -84,7 +87,7 @@ class Carousel extends Component {
                 })}
 
                 <div class="arrows" role="presentation">
-                    <div class="arrow-wrap" onClick={this.handleClick} role="presentation"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="arrow" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg></div>
+                    <div class={currentMovieIndex === results.length - 1 ? "disabled" : "arrow-wrap"} onClick={this.handleRight} role="presentation"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" class="arrow" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M5.622.631A2.153 2.153 0 0 0 5 2.147c0 .568.224 1.113.622 1.515l8.249 8.34-8.25 8.34a2.16 2.16 0 0 0-.548 2.07c.196.74.768 1.317 1.499 1.515a2.104 2.104 0 0 0 2.048-.555l9.758-9.866a2.153 2.153 0 0 0 0-3.03L8.62.61C7.812-.207 6.45-.207 5.622.63z"></path></svg></div>
                 </div>
             </div>
 
