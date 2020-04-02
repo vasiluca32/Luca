063206ec66cd46cc724a819843ee1c649edf4f52
vasiluca32 <vasilelucaluca@gmail.com>
2020-04-02 18:25:51 +0300

    rating carousel only on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Poster from "../components/Poster";
+import Main from "../components/main/rating-carousel/Carousel"
 
 class Home extends Component {
   render() {
@@ -8,7 +9,8 @@ class Home extends Component {
         <div className="container">
           <h2> Welcome to HOME Page</h2>
         </div>
-        <Poster/>
+        <Poster />
+        <Main />
       </div>
     );
   }
