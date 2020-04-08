bef2bf051f5baa3cb3bb4410465017c8baac2384
Delia Popescu <32332436+deliacpopescu@users.noreply.github.com>
2020-04-08 17:59:05 +0300

    from me to you (#12)
    Co-authored-by: Stefan Radusi <stefan.radusi@aeratechnology.com>

---
+++ src/components/Body.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Action from "../pages/Action";
+import Comedy from "../pages/Comedy";
+import Scifi from "../pages/Scifi";
+import Horror from "../pages/Horror";
+import Romance from "../pages/Romance";
+import Thriller from "../pages/Thriller";
+import Drama from "../pages/Drama";
+import Mystery from "../pages/Mystery";
+import Crime from "../pages/Crime";
+import Adventure from "../pages/Adventure";
+import Fantasy from "../pages/Fantasy";
+import Family from "../pages/Family";
+import AdvancedSearch from "../pages/AdvancedSearch";
+import "./Body.css";
+
+export function Body() {
+  return (
+    <div className="app-body">
+      <Route exact path="/" component={Home} />
+      <Route exact path="/Home" component={Home} />
+      <Route exact path="/ACTION" component={Action} />
+      <Route exact path="/COMEDY" component={Comedy} />
+      <Route exact path="/SCI-FI" component={Scifi} />
+      <Route exact path="/HORROR" component={Horror} />
+      <Route exact path="/ROMANCE" component={Romance} />
+      <Route exact path="/THRILLER" component={Thriller} />
+      <Route exact path="/DRAMA" component={Drama} />
+      <Route exact path="/MYSTERY" component={Mystery} />
+      <Route exact path="/CRIME" component={Crime} />
+      <Route exact path="/ANIMATION" component={Animation} />
+      <Route exact path="/ADVENTURE" component={Adventure} />
+      <Route exact path="/FANTASY" component={Fantasy} />
+      <Route exact path="/FAMILY" component={Family} />
+      <Route exact path="/Advanced Search" component={AdvancedSearch} />
+    </div>
+  );
+}
