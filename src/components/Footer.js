fb3928abfc1ac65684b04a23d5972efadf73d41f
hedi31 <heinrichhedviganiko@gmail.com>
2020-05-09 22:15:16 +0300

    adding about and help page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,52 @@
 import React from "react";
 import { MDBIcon, MDBContainer, MDBBtn } from "mdbreact";
 import "./Footer.css";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+class Footer extends Component {
+  render() {
+    return (
+      <div className="page-footer ">
+        <div className="container">
+          <div className="row text-center">
+            <MDBContainer>
+              <MDBBtn size="lg" tag="a" floating social="fb">
+                <MDBIcon fab icon="facebook-f" />
+              </MDBBtn>
+              <MDBBtn size="lg" tag="a" floating social="tw">
+                <MDBIcon fab icon="twitter" />
+              </MDBBtn>
+              <MDBBtn size="lg" tag="a" floating social="ins">
+                <MDBIcon fab icon="instagram" />
+              </MDBBtn>
+              <MDBBtn size="lg" tag="a" floating social="pin">
+                <MDBIcon fab icon="pinterest" />
+              </MDBBtn>
+              <MDBBtn size="lg" tag="a" floating social="yt">
+                <MDBIcon fab icon="youtube" />
+              </MDBBtn>
+            </MDBContainer>
+          </div>
+          <div className="row">
+            <Link className="col col-sm text-muted" to="/About">
+              <p className=" text-center">About</p>
+            </Link>
+            <Link className="col col-sm text-muted float-right" to="/Help">
+              <p className=" text-center ">Help</p>
+            </Link>
+          </div>
 
-const Footer = () => {
-  return (
-    <div className="page-footer ">
-      <div className="container">
-        <div className="row text-center">
-          <MDBContainer>
-            <MDBBtn size="lg" tag="a" floating social="fb">
-              <MDBIcon fab icon="facebook-f" />
-            </MDBBtn>
-            <MDBBtn size="lg" tag="a" floating social="tw">
-              <MDBIcon fab icon="twitter" />
-            </MDBBtn>
-            <MDBBtn size="lg" tag="a" floating social="ins">
-              <MDBIcon fab icon="instagram" />
-            </MDBBtn>
-            <MDBBtn size="lg" tag="a" floating social="pin">
-              <MDBIcon fab icon="pinterest" />
-            </MDBBtn>
-            <MDBBtn size="lg" tag="a" floating social="yt">
-              <MDBIcon fab icon="youtube" />
-            </MDBBtn>
-          </MDBContainer>
-        </div>
-        <div className="row">
-          <p className="col col-sm text-center">About</p>
-          <p className="col col-sm text-center">Help</p>
-        </div>
-
-        <hr />
-        <div className="row">
-          <p className="col col-sm text-center">
-            &copy; {new Date().getFullYear} Movie Website | All Right Reserved
-            To Saturn Team |Privacy Policy
-          </p>
+          <hr />
+          <div className="row">
+            <p className="col col-sm text-center">
+              &copy; {new Date().getFullYear} Movie Website | All Right Reserved
+              To Saturn Team |Privacy Policy
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Footer;
