402a85eaab4e432e640c170a6d1c13ddaadfffbc
vasiluca32 <vasilelucaluca@gmail.com>
2020-09-26 16:47:07 +0300

    undefined

--- src/components/main/rating-carousel/Carousel.js
+++ src/components/main/rating-carousel/Carousel.js
@@ -49,10 +49,15 @@ class Carousel extends Component {
                         <div className="arrow-wrap" role="presentation"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" onClick={this.handleLeft} className={currentMovieIndex === 0 ? "disabled" : "arrow"} viewBox="0 0 24 24" fill="currentColor" role="presentation"><path d="M18.378 23.369c.398-.402.622-.947.622-1.516 0-.568-.224-1.113-.622-1.515l-8.249-8.34 8.25-8.34a2.16 2.16 0 0 0 .548-2.07A2.132 2.132 0 0 0 17.428.073a2.104 2.104 0 0 0-2.048.555l-9.758 9.866A2.153 2.153 0 0 0 5 12.009c0 .568.224 1.114.622 1.515l9.758 9.866c.808.817 2.17.817 2.998-.021z"></path></svg></div>
                     </div>
                     {this.state.results.sort(function (a, b) { return b.imdbRating - a.imdbRating }).slice(currentMovieIndex, currentMovieIndex + 6).map((res) => {
+                        console.log(res) // poster does not exist in every movie
+
                         let moviePoster = NoPoster;
-                        let checkJpg = res.Poster.includes("jpg")
-                        let checkImg = res.Poster.includes("image")
-                        if (res.Poster !== "N/A" && (checkJpg || checkImg)) {
+                        let checkJpg = res.Poster.includes("jpg");
+                        let checkImg = res.Poster.includes("image");
+                        if (res.Poster === undefined) {
+                            moviePoster = NoPoster;
+                        }
+                        else if (res.Poster !== "N/A" && (checkJpg || checkImg)) {
                             moviePoster = res.Poster;
                         }
                         return (
