c5939375ae9545bc5e7e7112ef5687525335dacf
vasiluca32 <vasilelucaluca@gmail.com>
2020-09-27 15:55:12 +0300

    Updates

---
+++ precache-manifest.36bb39663dd148e155314f86ec898175.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064deb64bb2042d5d5fa6b04dfdb060e",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "a054584651b86ed0e433",
+    "url": "/movie/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "afe3759b379492f3ccd4",
+    "url": "/movie/static/css/main.1177895f.chunk.css"
+  },
+  {
+    "revision": "a054584651b86ed0e433",
+    "url": "/movie/static/js/2.1cd912a5.chunk.js"
+  },
+  {
+    "revision": "870e3c5359799be6ca7ed5db7d034ed4",
+    "url": "/movie/static/js/2.1cd912a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe3759b379492f3ccd4",
+    "url": "/movie/static/js/main.2f9cf20f.chunk.js"
+  },
+  {
+    "revision": "ed84cc0093c15cee011f",
+    "url": "/movie/static/js/runtime-main.2bc06d1f.js"
+  },
+  {
+    "revision": "bb881c107321bc87d7f6d4a5a05ad1c6",
+    "url": "/movie/static/media/Logo.bb881c10.png"
+  },
+  {
+    "revision": "205108d357e3b7d5fc5e461214be4bab",
+    "url": "/movie/static/media/NoPoster.205108d3.png"
+  },
+  {
+    "revision": "509e3cd34f78ecf2568361440a67b21a",
+    "url": "/movie/static/media/star.509e3cd3.png"
+  }
+]);
