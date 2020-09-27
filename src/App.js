1fae45f5bb6f6eb02c991f92efc64f9fdfb1818a
vasiluca32 <vasilelucaluca@gmail.com>
2020-09-27 15:37:54 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Body } from "./components/Body";
-import { HashRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 class App extends Component {
 
   render() {
     return (
-      <HashRouter>
+      <Router>
         <div className="App">
           <Header />
           <Body />
           <Footer />
         </div>
-      </HashRouter>
+      </Router>
     );
   }
 }
